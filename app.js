5fc07ba0138e8299297ee3e022cb2807fb1e7f43
pazliddinovulugbek <pazliddinovulugbek07@gmail.com>
2024-10-22 17:12:53 +0500

    full

---
+++ app.js
@@ -0,0 +1,9 @@
+const peopleName = []
+
+peopleName[0] = prompt("Add a person")
+peopleName[1] = prompt("Add a person")
+peopleName[2] = prompt("Add a person")
+peopleName[3] = prompt("Add a person")
+
+
+console.log(peopleName);
